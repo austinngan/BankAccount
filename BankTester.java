d68ce2f58a9e55f5567ba1301d11515809a08ba7
austinngan <angan20@stuy.edu>
2020-10-27 13:32:58 -0400

    Added tests for password and adding a new password

--- BankTester.java
+++ BankTester.java
@@ -1,7 +1,10 @@
 public class BankTester{
   public static void main(String[]args){
     BankAccount a1=new BankAccount(68416516, "981754AF165");
-    System.out.println(a1.getID());
-    System.out.println(a1.getBal());
+    System.out.println(a1.getAccountID());
+    System.out.println(a1.getBalance());
+    System.out.println(a1.getPassword());
+    a1.setPassword("718423hF2");
+    System.out.println(a1.getPassword());
   }
 }
