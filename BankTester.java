0c1d294287f6a902d0648643d1ba3528f8ffe99d
austinngan <angan20@stuy.edu>
2020-10-27 13:21:39 -0400

    Tester now returns values for balance and id

--- BankTester.java
+++ BankTester.java
@@ -1,5 +1,7 @@
-public class RandomArrayTester{
+public class BankTester{
   public static void main(String[]args){
-    BankAccount a1=new BankAccount(68416516, 981754AF165);
+    BankAccount a1=new BankAccount(68416516, "981754AF165");
+    System.out.println(a1.getID());
+    System.out.println(a1.getBal());
   }
 }
