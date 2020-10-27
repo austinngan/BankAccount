542d6a032a1811626e957b01daed69b162c1bd54
austinngan <angan20@stuy.edu>
2020-10-27 13:12:47 -0400

    Added BankAccount constructor

---
+++ BankAccount.java
@@ -0,0 +1,10 @@
+public class BankAccount{
+  private double balance;
+  private int accountID;
+  private String password;
+  public BankAccount(int id; String pw){
+    balance=0.0;
+    accountID=id;
+    password=pw;
+  }
+}
